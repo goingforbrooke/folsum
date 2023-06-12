73589190fc4e03a9237c19a9aa60497347f7829c
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-06-11 21:14:33 -0700

    Add Zsh script used to generate resized icons

---
+++ images/icons/resize_icons.zsh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+# Source for pixel sizes: https://developer.apple.com/design/human-interface-guidelines/app-icons#macOS-app-icon-sizes
+
+# Input icon file
+input_icon="./folsum_icon.png"
+
+# Output directory for resized icons
+output_dir="resized_icons"
+
+# Ensure that the output directory exists.
+mkdir ${output_dir}
+
+# Array of @1x pixel icon sizes (in pixels) for macOS
+sizes=(16 32 128 256 512)
+
+# Loop through each @1x size and resize the icon
+for size in "${sizes[@]}"; do
+  output_file="$output_dir/folsum_icon_${size}px.png"
+  convert "$input_icon" -resize "${size}x${size}" "$output_file"
+done
+
+# Array of @2x pixel icon sizes (in pixels) for macOS
+sizes=(32 64 128 256 512 1024)
+
+# Loop through each @2x size and resize the icon
+for size in "${sizes[@]}"; do
+  output_file="$output_dir/folsum_icon_${size}px@2x.png"
+  convert "$input_icon" -resize "${size}x${size}" "$output_file"
+done
+
