ae0cdd4be7abe78efdef84db0c297cc385e222f2
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-07-16 19:34:18 -0700

    Refactor: shadow `export_file` more

--- folsum/src/app.rs
+++ folsum/src/app.rs
@@ -226,11 +226,11 @@ impl eframe::App for TemplateApp {
                             csv_rows.push_str(&csv_row)
                         }
                         // Lock the export file path so we can use it to create the CSV dump.
-                        let unlocked_export_file = export_file.lock().unwrap();
+                        let export_file = export_file.lock().unwrap();
                         // Clone user's chosen export path so we can release it's lock, allowing live table updates.
-                        let export_file_copy = unlocked_export_file.clone().unwrap();
+                        let export_file = export_file.clone().unwrap();
                         // Create a CSV file to write the extension types and their counts to, overwriting it if it already exists.
-                        let mut csv_export = File::create(export_file_copy).expect("Failed to create CSV export file");
+                        let mut csv_export = File::create(export_file).expect("Failed to create CSV export file");
                         // Write the CSV's content to the export file.
                         csv_export.write_all(csv_rows.as_bytes()).expect("Failed to write contents to CSV export file")
                     });
