9f98b63f057bd5d96bf8b294a1c8bbdcb2780657
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-07-28 12:55:59 -0700

    Show a live update of extension counts in the GUI

--- folsum/src/main.rs
+++ folsum/src/main.rs
@@ -1,14 +1,17 @@
 // Standard library libraries.
 use std::collections::HashMap;
-use std::env;
 use std::ffi::OsStr;
 use std::ffi::OsString;
 use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::{Duration, Instant};
 
 // Iced GUI libraries.
 use iced::executor;
 use iced::widget::{button, container, text, Column, Row, scrollable};
-use iced::{Application, Command, Element, Length, Settings, Theme};
+use iced::{Application, Command, Element, Length, Settings, Theme, time, Subscription};
+
+use dirs::home_dir;
 
 
 use itertools::Itertools;
@@ -17,7 +20,7 @@ use itertools::Itertools;
 use walkdir::WalkDir;
 
 // Local modules.
-mod download;
+//mod download;
 
 pub fn main() -> iced::Result {
     // Start the GUI.
@@ -32,9 +35,16 @@ struct FolsumGui {
     state: SummarizationState,
 }
 
+#[derive(Debug)]
+pub enum SummarizationState {
+    Idle,
+    Summarizing,
+}
+
 #[derive(Debug, Clone)]
 pub enum Message {
     StartSummarizing,
+    DisplayCounts(Instant),
 }
 
 impl Application for FolsumGui {
@@ -48,6 +58,7 @@ impl Application for FolsumGui {
         (
             FolsumGui {
                 extension_counts: Arc::new(Mutex::new(HashMap::new())),
+                state: SummarizationState::Idle,
             },
             Command::none(),
         )
@@ -60,17 +71,80 @@ impl Application for FolsumGui {
 
     fn update(&mut self, message: Message) -> Command<Message> {
         match message {
-            Message::StartSummarizing => {
-                // Reset file extension counts to zero.
-                *self.extension_counts.lock().unwrap() = HashMap::new();
+            Message::StartSummarizing => match self.state {
+                SummarizationState::Idle => {
+                    self.state = SummarizationState::Summarizing;
+                    // Reset file extension counts to zero.
+                    *self.extension_counts.lock().unwrap() = HashMap::new();
+                    // Copy the Arcs of persistent members so they can be accessed by a separate thread.
+                    let extension_counts_copy = Arc::clone(&self.extension_counts);
+                    thread::spawn(move || {
+                        let default_extension = OsString::from("No extension");
+                        // Recursively iterate through each subdirectory and don't add subdirectories to the result.
+                        for entry in WalkDir::new(home_dir().unwrap())
+                            .min_depth(1)
+                            .into_iter()
+                            .filter_map(Result::ok)
+                            .filter(|e| !e.file_type().is_dir())
+                        {
+                            // Extract the file extension from the file's name.
+                            let file_ext: &OsStr = entry.path().extension().unwrap_or(&default_extension);
+                            let show_ext: String = String::from(file_ext.to_string_lossy());
+                            // Lock the extension counts variable so we can add a file to it.
+                            let mut unlocked_counts_copy = extension_counts_copy.lock().unwrap();
+                            // Add newly encountered file extensions to known file extensions with a counter of 0.
+                            let counter: &mut u32 = unlocked_counts_copy.entry(show_ext).or_insert(0);
+                            // Increment the counter for known file extensions by one.
+                            *counter += 1;
+                            drop(unlocked_counts_copy);
+                        }
+                    });
+                }
+                SummarizationState::Summarizing => {
+                    // Reset file extension counts to zero.
+                    *self.extension_counts.lock().unwrap() = HashMap::new();
+                    // Copy the Arcs of persistent members so they can be accessed by a separate thread.
+                    let extension_counts_copy = Arc::clone(&self.extension_counts);
+                    let default_extension = OsString::from("No extension");
+                    for entry in WalkDir::new(home_dir().unwrap())
+                        .min_depth(1)
+                        .into_iter()
+                        .filter_map(Result::ok)
+                        .filter(|e| !e.file_type().is_dir())
+                    {
+                        // Extract the file extension from the file's name.
+                        let file_ext: &OsStr = entry.path().extension().unwrap_or(&default_extension);
+                        let show_ext: String = String::from(file_ext.to_string_lossy());
+                        // Lock the extension counts variable so we can add a file to it.
+                        let mut unlocked_counts_copy = extension_counts_copy.lock().unwrap();
+                        // Add newly encountered file extensions to known file extensions with a counter of 0.
+                        let counter: &mut u32 = unlocked_counts_copy.entry(show_ext).or_insert(0);
+                        // Increment the counter for known file extensions by one.
+                        *counter += 1;
+                        drop(unlocked_counts_copy);
+                    }
+                }
+            }
+            Message::DisplayCounts(now) => {
                 // Copy the Arcs of persistent members so they can be accessed by a separate thread.
                 let extension_counts_copy = Arc::clone(&self.extension_counts);
                 // Lock the extension counts variable so we can read it.
-                let mut unlocked_counts_copy = extension_counts_copy.lock().unwrap();
+                //let mut unlocked_counts_copy = extension_counts_copy.lock().unwrap();
             }
         };
 
         Command::none()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        match self.state {
+            // If nothing's being summarized, then there are no summarization updates to subscribe to.
+            SummarizationState::Idle => Subscription::none(),
+            // If a directory's being summarized, then wait ten milliseconds before doing anything, produce a message that file extension counts have been updated, then wait ten milliseconds.
+            SummarizationState::Summarizing { .. } => {
+                time::every(Duration::from_millis(10)).map(Message::DisplayCounts)
+            }
+        }
     }
 
     fn view(&self) -> Element<Message> {
