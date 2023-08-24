3c0784b78eadd07519a23ae566c9ce58278861b0
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-08-23 18:27:34 -0700

    Test: Ensure that summarization counts are accurate

--- folsum/src/summarize.rs
+++ folsum/src/summarize.rs
@@ -67,3 +67,73 @@ pub fn summarize_directory(summarization_path: &Arc<Mutex<Option<PathBuf>>>,
     };
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    // Test-related dependencies.
+    use std::collections::HashMap;
+    use std::fs;
+    use std::path::PathBuf;
+    use std::sync::{Arc, Mutex};
+    use std::thread;
+
+    use web_time::{Duration, Instant};
+
+    use crate::summarize_directory;
+
+
+    fn generate_mock_directory(test_dir: &PathBuf) -> std::io::Result<HashMap<String, u32>> {
+        let mut current_path = test_dir.clone();
+        // Create a test directory in the current directory.
+        fs::create_dir(&current_path)?;
+        let extensions = vec!["py", "pdf", "doc", "zip", "xml"];
+        let mut extension_counts: HashMap<String, u32> = HashMap::new();
+        for subdir_depth in 1..=10 {
+            current_path.push(format!("subdir_{}", subdir_depth));
+            fs::create_dir(&current_path)?;
+            for counter in 1..subdir_depth {
+                // Pick the file extension for this dummy file based off of how deep the subdir is.
+                let extension = &extensions[counter % extensions.len()];
+                let filename = format!("file_{}.{}", counter, extension);
+                let file_path = current_path.join(filename);
+                // Create the empty file.
+                fs::File::create(file_path)?;
+                // If the file extension already exists, then add one to it, otherwise, add it with "zero."
+                *extension_counts.entry(extension.to_string()).or_insert(0) += 1;
+            }
+        }
+        Ok(extension_counts)
+    }
+
+    #[test]
+    fn test_summarization() {
+        let test_dir = PathBuf::from("test_dir");
+        // Mock some subdirectories that contain various files with different extensions.
+        let mock_directory = generate_mock_directory(&test_dir).unwrap();
+
+        // Mock global state mutated by `summarize_directory`.
+        let extension_counts = Arc::new(Mutex::new(HashMap::new()));
+        let summarization_path = Arc::new(Mutex::new(Some(test_dir.clone())));
+        let summarization_start = Arc::new(Mutex::new(Instant::now()));
+        let time_taken = Arc::new(Mutex::new(Duration::ZERO));
+
+        let _summarization_result = summarize_directory(&summarization_path,
+                                                        &extension_counts,
+                                                        &summarization_start,
+                                                        &time_taken);
+        // Wait a bit so the summarization thread has a chance to do it's thing.
+        thread::sleep(Duration::from_secs(1));
+        let unlocked_extension_counts = extension_counts.lock().unwrap();
+        for (found_extension, counts) in unlocked_extension_counts.iter() {
+            println!("ext: {}", found_extension);
+            println!("attempt: {}", counts);
+            println!("answer: {}", &mock_directory[found_extension]);
+            assert_eq!(&mock_directory[found_extension], counts);
+        }
+        println!("mocked contents: {:?}", mock_directory);
+        println!("summarized contents: {:?}", unlocked_extension_counts);
+        // Cleanup: Recursively remove mocked subdirectories.
+        let _delete_result = fs::remove_dir_all(&test_dir);
+    }
+
+}
