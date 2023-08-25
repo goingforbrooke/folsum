44a6f23823d6538c838233f5d51a712ee897ceaf
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-08-25 14:28:33 -0700

    Add helper for reading CSV exports

--- folsum/tests/integration_test.rs
+++ folsum/tests/integration_test.rs
@@ -1,6 +1,7 @@
 // Test-related dependencies.
 use std::collections::HashMap;
-use std::fs;
+use std::fs::{self, File};
+use std::io::{self, BufRead, BufReader};
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -11,7 +12,7 @@ use folsum;
 
 
 #[test]
-fn test_summarization() {
+fn test_summarization_and_export() {
     // Create the test directory in `./test-dir`.
     let test_dir = PathBuf::from("test_dir");
     // Mock some subdirectories that contain various files with different extensions.
@@ -35,9 +36,38 @@ fn test_summarization() {
     for (found_extension, counts) in unlocked_extension_counts.iter() {
         assert_eq!(&answer_key[found_extension], counts);
     }
-    // todo: Clean up test directories whether tests fail or succeed.
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    
+    // Mock the export filename as if the investigator named the file `export_test`
+    let export_file = Arc::new(Mutex::new(Some(PathBuf::from("export_test.csv"))));
+    // Export summarization results of the mocked directory to CSV.
+    let _result = folsum::export_csv(&export_file, &extension_counts);
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    
+    // todo: Clean up mocked test directory whether tests fail or succeed.
     // Cleanup: Recursively remove mocked subdirectories.
     let _delete_result = fs::remove_dir_all(&test_dir);
+}
+
+fn read_csv_to_hashmap(filename: &str) -> io::Result<HashMap<String, u32>> {
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+    let mut extension_counts: HashMap<String, u32> = HashMap::new();
+    // For each line in the CSV export...
+    for line in reader.lines() {
+        let line = line?;
+        // Separate each line on commas.
+        let mut parts = line.splitn(2, ',');
+        // Assume that the extension name is the first part of the line.
+        let extension_name = parts.next().unwrap().to_string();
+        // Assume that the number of times the extension was seen is the first part of the line.
+        let raw_occurrences: &str = parts.next().unwrap();
+        let extension_occurrences: u32 = raw_occurrences.parse::<u32>().unwrap();
+        extension_counts.insert(extension_name, extension_occurrences);
+    }
+    Ok(extension_counts)
 }
 
 pub fn generate_mock_directory(test_dir: &PathBuf) -> std::io::Result<HashMap<String, u32>> {
