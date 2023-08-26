eafe69e05425d6659baa135778867821ba2b530b
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-08-25 20:03:24 -0700

    Fix: Release mutex lock on ext counts asap

--- folsum/tests/integration_test.rs
+++ folsum/tests/integration_test.rs
@@ -31,9 +31,8 @@ fn test_summarization_and_export() {
                                                             &time_taken);
     // Wait a bit so the summarization thread has a chance to do it's thing.
     thread::sleep(Duration::from_secs(1));
-    let unlocked_extension_counts = extension_counts.lock().unwrap();
     // Summarization test.
-    for (found_extension, counts) in unlocked_extension_counts.iter() {
+    for (found_extension, counts) in extension_counts.lock().unwrap().iter() {
         // For each summarized file extension, ensure that the number of files found matches the actual number of files.
         assert_eq!(&actual_extensions[found_extension], counts);
     }
