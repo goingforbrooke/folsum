4565d681d64e828e7af2039d6183126588ccb54d
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-08-25 22:00:24 -0700

    Fix: Clone export filename to prevent borrow err

--- folsum/tests/integration_test.rs
+++ folsum/tests/integration_test.rs
@@ -45,7 +45,7 @@ fn test_summarization_and_export() {
     // Export summarization results of the mocked directory to CSV.
     let _export_result = folsum::export_csv(&mocked_export_file, &extension_counts);
     // Extract the content of the exported CSV.
-    let exported_counts = read_csv_to_hashmap(export_file);
+    let exported_counts = read_csv_to_hashmap(export_file.clone());
     // For each file extension, ensure that the number of files found 
     for (summarized_extension, counts) in exported_counts.unwrap().iter() {
         // For each exported file extension, ensure that the number of files found matches the actual number of files.
