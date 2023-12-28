04fffec4313878510df437e92cd91e3c8232ff59
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-12-28 12:11:12 -0800

    Simplify logger initialization

--- folsum/src/main.rs
+++ folsum/src/main.rs
@@ -1,16 +1,23 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use log::info;
+use log::{debug, error, info, warn};
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
-    let mut logger_builder = env_logger::Builder::from_env(env_logger::Env::default()).init();
+    // Define how messages should be logged.
+    let logger_env = env_logger::Env::default();
+    env_logger::init_from_env(logger_env);
+
     // Don't show egui log messages because FolSum's debugs gets lost in the sea of egui debugs.
     //logger_builder.filter_module("egui", LevelFilter::Info);
 
     info!("Initialized logger");
+    debug!("Initialized logger");
+    error!("Initialized logger");
+    info!("Initialized logger");
+    warn!("Initialized logger");
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
