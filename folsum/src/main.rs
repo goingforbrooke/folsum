5b90f9318f685e51ac371e4d4eda7d3374502f8f
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-12-28 16:15:09 -0800

    Log: Ignore non-warning logs: egui glow and winit

--- folsum/src/main.rs
+++ folsum/src/main.rs
@@ -45,8 +45,12 @@ fn setup_native_logging() -> Result<(), Box<dyn Error>> {
                 message = message,
             ));
         })
-        // Ignore all non-warning eframe logs in console.
+        // Ignore all non-warning `eframe` logs in console.
         .level_for("eframe", log::LevelFilter::Warn)
+        // Ignore all non-warning `egui_glow` logs in console.
+        .level_for("egui_glow", log::LevelFilter::Warn)
+        // Ignore all non-warning `egui_winit` logs in console.
+        .level_for("egui_winit", log::LevelFilter::Warn)
         // Console log records at DEBUG and above.
         .level(log::LevelFilter::Debug)
         // Send unfiltered messages to stdout.
