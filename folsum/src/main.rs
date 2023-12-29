49aa39bb2144139372562d622febe4d6e8a4690c
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-12-28 18:20:19 -0800

    Log: Remove extra spaces around emojis

--- folsum/src/main.rs
+++ folsum/src/main.rs
@@ -34,7 +34,7 @@ fn setup_native_logging() -> Result<(), Box<dyn Error>> {
     let stdout_config = fern::Dispatch::new()
         .format(move |out, message, record| {
             out.finish(format_args!(
-                "[{timestamp} {level_emoji} {record_filename}L{record_line}::{record_module}] {color_line}{message}\x1B[0m",
+                "[{timestamp}{level_emoji}{record_filename}L{record_line}::{record_module}] {color_line}{message}\x1B[0m",
                 color_line = format_args!(
                     "\x1b[{}m",
                     colors_line.get_color(&record.level()).to_fg_str()
