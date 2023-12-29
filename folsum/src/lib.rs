62c41c5fc141b5b9621246f75dac164e9a830c3e
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-12-28 18:42:43 -0800

    Alphabetize items in `lib.rs`

--- folsum/src/lib.rs
+++ folsum/src/lib.rs
@@ -1,10 +1,10 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
-mod gui;
-pub use gui::FolsumGui;
-
 mod export_csv;
 pub use export_csv::export_csv;
+
+mod gui;
+pub use gui::FolsumGui;
 
 mod summarize;
 pub use summarize::summarize_directory;
