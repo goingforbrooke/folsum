7237362127ea6a508ba1bcdf0faf718a703059a6
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-12-28 16:31:18 -0800

    Log: Restrict colorization to level and message

--- folsum/src/main.rs
+++ folsum/src/main.rs
@@ -33,13 +33,14 @@ fn setup_native_logging() -> Result<(), Box<dyn Error>> {
     let stdout_config = fern::Dispatch::new()
         .format(move |out, message, record| {
             out.finish(format_args!(
-                "{color_line}[{date} {level} {target} {color_line}] {message}\x1B[0m",
+                "[{date} {color_line} {level} {target} {color_line}] {message}\x1B[0m",
                 color_line = format_args!(
                     "\x1b[{}m",
                     colors_line.get_color(&record.level()).to_fg_str()
                 ),
                 date = humantime::format_rfc3339_seconds(SystemTime::now()),
                 // Colorize the log record based off of its log level.
+                // todo: Add filename and line number.
                 target = record.target(),
                 level = colors_line.color(record.level()),
                 message = message,
