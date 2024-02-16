71f48f7efdd983b37112994d344f79acce686d06
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2024-02-16 10:17:31 -0800

    Move `mod.rs` to top-level

---
+++ folsum/src/mod.rs
@@ -0,0 +1,16 @@
+pub struct TempEnvVar {
+    variable_name: &'static str,
+}
+
+impl TempEnvVar {
+    pub fn new(variable_name: &'static str, desired_value: &str) -> Self {
+        std::env::set_var(variable_name, desired_value);
+        Self { variable_name }
+    }
+}
+
+impl Drop for TempEnvVar {
+    fn drop(&mut self) {
+        std::env::remove_var(self.variable_name);
+    }
+}
--- folsum/src/tests/common/mod.rs
+++
@@ -1,3 +0,0 @@
-pub struct EnvVarGuard {
-    key: str,
-}
