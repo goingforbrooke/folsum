2cabc434ff2b34a89c785712e6b66896f2e14893
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2024-02-16 12:45:53 -0800

    Add `'a` struct lifetime parameters to test util

--- folsum/src/common.rs
+++ folsum/src/common.rs
@@ -1,17 +1,19 @@
 #[cfg(test)]
 pub mod test_utilities {
-    pub struct TempEnvVar {
-        variable_name: &'static str,
+    use std::error::Error;
+
+    pub struct TempEnvVar<'a> {
+        variable_name: &'a str,
     }
 
-    impl TempEnvVar {
-        pub fn new(variable_name: &'static str, desired_value: &str) -> Self {
+    impl<'a> TempEnvVar<'a> {
+        pub fn new(variable_name: &'a str, desired_value: &'a str) -> Self {
             std::env::set_var(variable_name, desired_value);
             Self { variable_name }
         }
     }
 
-    impl Drop for TempEnvVar {
+    impl<'a> Drop for TempEnvVar<'a> {
         fn drop(&mut self) {
             std::env::remove_var(self.variable_name);
         }
