db139a7207a3106c60f764e900b0d21c7d51d367
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2025-03-12 10:22:30 -0700

    Bench: Add summarization benchmakr invocation

--- folsum/benches/benchmark_directory_summarization.rs
+++ folsum/benches/benchmark_directory_summarization.rs
@@ -1,3 +1,11 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use folsum::run_fake_summarization;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("benchmark_directory_summarization",
+                     |b| b.iter(|| run_fake_summarization()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
