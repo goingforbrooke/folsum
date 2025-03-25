cea9665b8a3be821261973170ee3ffe9c62bd7a8
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2025-03-24 21:27:00 -0700

    Revert "Test: Remove helper `TempHomeEnvVar`"
    This reverts commit c4aaa828b588397903ba8203b87caa26e22b36fd.

--- folsum/src/common.rs
+++ folsum/src/common.rs
@@ -16,6 +16,7 @@ macro_rules! debug_println {
 
 pub const CSV_HEADERS: &str = "File Path, MD5 Hash\n";
 
+
 // Point in the summarization process of a directory's contents.
 #[derive(Clone)]
 pub enum SummarizationStatus {
@@ -23,6 +24,7 @@ pub enum SummarizationStatus {
     InProgress,
     Done,
 }
+
 
 /// Integrity of the whole directory being summarized.
 #[derive(Clone, Debug)]
@@ -67,6 +69,56 @@ impl FoundFile {
             file_path,
             md5_hash,
             file_verification_status: FileIntegrity::default(),
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod test_utilities {
+    use anyhow::{bail, Result};
+
+    pub struct TempHomeEnvVar {
+        variable_name: String,
+    }
+
+    impl TempHomeEnvVar {
+        pub fn new(desired_value: &str) -> Result<Self> {
+            let variable_name = Self::get_platform_env_var()?;
+            std::env::set_var(&variable_name, desired_value);
+            debug_println!(
+                "Added temporary env var: {}:{}",
+                &variable_name,
+                &desired_value
+            );
+            Ok(Self { variable_name })
+        }
+
+        /// Get platform-specifc environment variable that corresponds to `$HOME`.
+        pub fn get_platform_env_var() -> Result<String> {
+            let platform = if cfg!(unix) {
+                "unix"
+            } else if cfg!(windows) {
+                "windows"
+            } else {
+                "unknown"
+            };
+            let env_var_name = match platform {
+                "unix" => "HOME",
+                "windows" => "USERPROFILE",
+                // todo: Raise more specific test util (?Anyhow?) setup error for unknown platform.
+                _ => bail!("Unsupported platform"),
+            };
+            Ok(env_var_name.to_string())
+        }
+    }
+
+    impl Drop for TempHomeEnvVar {
+        fn drop(&mut self) {
+            std::env::remove_var(&self.variable_name);
+            debug_println!(
+                "Automatically removed temporary env var: {}",
+                self.variable_name
+            );
         }
     }
 }
